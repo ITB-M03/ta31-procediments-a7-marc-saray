69887110d8d9e9aeea188959ccefee1812b93b68
marc.cuenca.7e8 <marc.cuenca.7e8@itb.cat>
2024-12-13 10:43:31 +0100

    Commit:

--- src/main/kotlin/controllers/Ejercicio_5/Ejercicio_5.kt
+++ src/main/kotlin/controllers/Ejercicio_5/Ejercicio_5.kt
@@ -1,10 +1,9 @@
 package controllers.Ejercicio_5
 
-import controllers.Ejercicio_4.ponerTexto
+import controllers.Exe1.scan
 import utilities.abrirScanner
 import utilities.cerrarScanner
 import java.util.*
-import kotlin.collections.List
 
 fun main(){
 
@@ -15,23 +14,49 @@ fun main(){
     println("Bienvenido al Menu")
     val numero = pedirNumero("Pon un numero del 1 al 4",scan)
 
+    //Llamamos a la funcion "Menu"
+    val lista = menu(numero)
 
 
     //LLamamos a la funcion de cerrar scanner
     cerrarScanner(scan)
 
 }
-//Creamos la funcion de pedir numero
-fun pedirNumero (msg:String,  scan: Scanner): Int{
-
-    //Imprimimos la frase que leera el usuario
+fun pedirNumero (msg: String, numero: Int, scan: Scanner):Int{
+    //Le imprimos el mensaje por pantalla
     println(msg)
 
-    //Leemos el numero que pondra el usuario
-    val numerousuario = scan.nextInt()
+    //El usuario da un numero
+    val numero_user = scan.nextInt()
 
-    //Devolvemos el numero del usuario
-    return numerousuario
+    //Devolvemos el valor
+    return numero_user
+
 }
+
 //Hacemos una funcion para hacer el menu
-fun menu ()
+fun menu (msg:String, numero:Int, scan: Scanner): Int {
+
+    //Hacemos un when que si el usuario da un numero del 1 al 4 llamara a una funcion
+    when(numero){
+        1 -> {
+            push()
+        }
+        2 -> {
+            pop()
+        }
+        3 -> {
+            pila()
+        }
+        4 ->{
+            salir()
+        }
+        else ->{
+            println("Ese numero no vale, pon otro")
+        }
+    }
+
+    return numero
+
+
+}
