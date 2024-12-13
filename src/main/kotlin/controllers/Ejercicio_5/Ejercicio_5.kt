806913e84ab51fc6bba8dc00d1993bcc8037087c
marc.cuenca.7e8 <marc.cuenca.7e8@itb.cat>
2024-12-13 13:29:43 +0100

    Commit:

--- src/main/kotlin/controllers/Ejercicio_5/Ejercicio_5.kt
+++ src/main/kotlin/controllers/Ejercicio_5/Ejercicio_5.kt
@@ -4,111 +4,104 @@ import utilities.abrirScanner
 import utilities.cerrarScanner
 import java.util.*
 
-data class Pila (
-    val elementos: Array<Int> = Array<Int>(10){ 0 },
-    var top : Int = 0
+data class Pila(
+    val elementos: Array<Int> = Array(10) { 0 },
+    var top: Int = 0
 )
 
 fun main() {
-    // Llamamos a la función de abrir scanner
     val scan: Scanner = abrirScanner()
-
     val pila = crearPila()
 
-    // Llamamos a la función "pedirNumero"
     println("Bienvenido al Menú")
+    iniciarMenu(pila, scan)
 
-    //Funcion para mostrar menu
-    listadoMenu()
-
-    val numero = pedirNumero("Pon un número del 1 al 4: ", scan)
-
-
-    // Llamamos a la función "Menu"
-    val lista = menu(numero, pila, scan)
-
-
-
-    // Llamamos a la función de cerrar scanner
     cerrarScanner(scan)
 }
 
-// Función para pedir un número al usuario
-fun pedirNumero(msg: String, scan: Scanner): Int {
-    // Imprimimos el mensaje por pantalla
-    println(msg)
-
-    // El usuario da un número
-    val numero_user = scan.nextInt()
-
-    // Devolvemos el valor
-    return numero_user
-}
-//Hacemos una funcion para hacer el menu
-fun menu(numero: Int, pila: Pila, scan: Scanner) {
-
+fun iniciarMenu(pila: Pila, scan: Scanner) {
     var salir = false
 
     while (!salir) {
+        listadoMenu()
+        val opcion = pedirNumero("Pon un número del 1 al 4: ", scan)
+        salir = procesarOpcion(opcion, pila, scan)
+    }
 
-        when (numero) {
-            1 -> {
-                println("Introduce un numero para agregarlo")
-                push(pila,scan.nextInt())
-            }
+    println("Contenido final de la pila:")
+    mostrar(pila)
+}
 
-            2 -> {
-                pop(pila)
-            }
-
-            3 -> {
-                mostrar(pila)
-            }
-
-            4 -> {
-                salir = true
-            }
-
-            else -> {
-            }
+fun procesarOpcion(opcion: Int, pila: Pila, scan: Scanner): Boolean {
+    return when (opcion) {
+        1 -> {
+            val numero = pedirNumero("Introduce un número para agregarlo: ", scan)
+            push(pila, numero)
+            false
+        }
+        2 -> {
+            pop(pila)
+            false
+        }
+        3 -> {
+            mostrar(pila)
+            false
+        }
+        4 -> {
+            println("Saliendo del programa...")
+            true
+        }
+        else -> {
+            println("Opción no válida, por favor intenta de nuevo.")
+            false
         }
     }
 }
 
-fun crearPila (): Pila {
-    val pila = Pila()
-    return pila
-}
-fun push (pila: Pila, numero: Int){
-    if (pila.top == 10){
-        println("Pila llena")
-    } else {
-        pila.elementos[pila.top] = numero
-        pila.top++
-    }
-}
-fun pop (pila: Pila){
-    pila.elementos[pila.top] = 0
-    pila.top --
+fun crearPila(): Pila {
+    return Pila()
 }
 
-fun mostrar(pila: Pila){
-    for (i in pila.elementos.indices){
-        if (pila.elementos[i] != 0){
-            println(pila.elementos[i])
-        }
-    }
+fun pedirNumero(msg: String, scan: Scanner): Int {
+    print(msg)
+    return scan.nextInt()
 }
 
-//Creamos la funcion de imprimir el menu
-fun listadoMenu (){
+fun listadoMenu() {
     println("1.- Afegir número (push)")
     println("2.- Treure número (pop)")
     println("3.- Mostrar contingut de la pila")
     println("4.- Sortir")
 }
 
-//Creamos la funcion mostrar pila
-fun imprimir(pila: Pila){
-    println(pila.elementos.contentToString())
+fun push(pila: Pila, numero: Int) {
+    if (pila.top == 10) {
+        println("La pila está llena. No puedes agregar más elementos.")
+    } else {
+        pila.elementos[pila.top] = numero
+        pila.top++
+        println("Se ha agregado el número $numero a la pila.")
+    }
+}
+
+fun pop(pila: Pila) {
+    if (pila.top == 0) {
+        println("La pila está vacía. No puedes quitar ningún elemento.")
+    } else {
+        pila.top--
+        val elementoEliminado = pila.elementos[pila.top]
+        pila.elementos[pila.top] = 0
+        println("Se ha quitado el número $elementoEliminado de la pila.")
+    }
+}
+
+fun mostrar(pila: Pila) {
+    if (pila.top == 0) {
+        println("La pila está vacía.")
+    } else {
+        println("Contenido de la pila:")
+        for (i in pila.top - 1 downTo 0) {
+            println(pila.elementos[i])
+        }
+    }
 }
